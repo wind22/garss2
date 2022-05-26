3ffad448fde846842e143208333548f6df7a8e56
wind22 <13938504754@163.com>
2022-05-26 16:04:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -160,9 +160,9 @@ def replace_readme():
                     if (rss_info_atom["date"] == datetime.today().strftime("%Y-%m-%d")):
                         new_num = new_num + 1
                         if (new_num % 2) == 0:
-                            current_date_news_index[0] = current_date_news_index[0] + "<div style='line-height:3;' ><a href='" + rss_info_atom["link"] + "' " + 'style="line-height:2;text-decoration:none;display:block;color:#584D49;">' + "🌈 ‣ " + rss_info_atom["title"] + " |author:" + rss_info_atom["author"] + " | 第" + str(new_num) +"篇" + "</a></div>"
+                            current_date_news_index[0] = current_date_news_index[0] + "<div style='line-height:3;' ><a href='" + rss_info_atom["link"] + "' " + 'style="line-height:2;text-decoration:none;display:block;color:#584D49;">' + "🌈 ‣ " + rss_info_atom["title"] + " |author:" + rss_info_atom["author"] + "</a></div>"
                         else:
-                            current_date_news_index[0] = current_date_news_index[0] + "<div style='line-height:3;background-color:#FAF6EA;' ><a href='" + rss_info_atom["link"] + "' " + 'style="line-height:2;text-decoration:none;display:block;color:#584D49;">' + "🌈 ‣ " + rss_info_atom["title"] + " |author:" + rss_info_atom["author"] + " | 第" + str(new_num) +"篇" + "</a></div>"
+                            current_date_news_index[0] = current_date_news_index[0] + "<div style='line-height:3;background-color:#FAF6EA;' ><a href='" + rss_info_atom["link"] + "' " + 'style="line-height:2;text-decoration:none;display:block;color:#584D49;">' + "🌈 ‣ " + rss_info_atom["title"] + " |author:" + rss_info_atom["author"] + "</a></div>"
 
             except:
                 print("An exception occurred")
